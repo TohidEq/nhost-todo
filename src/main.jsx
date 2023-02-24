8e8b7e30500084d3932f30fccb6bddb09fda7df7
TohidEq <TohidEq@gmail.com>
2023-02-24 12:50:02 +0330

    Get Data

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import nhost from "./utils/nhost";
+import { NhostProvider } from "@nhost/react";
+import { NhostApolloProvider } from "@nhost/react-apollo";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <NhostProvider nhost={nhost}>
+      <NhostApolloProvider nhost={nhost}>
+        <App />
+      </NhostApolloProvider>
+    </NhostProvider>
+  </React.StrictMode>
+);
